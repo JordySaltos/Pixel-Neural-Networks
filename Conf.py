db25ad04cb8a055bf1df0fe22d1a5a8b946b4fd0
JuancarlosPG2004 <j.pajaresalumno2014@gmail.com>
2026-03-18 15:18:06 +0100

    Documentation

---
+++ Conf.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../source'))  # añade la raíz del proyecto
+
+# -- Project information -----------------------------------------------------
+
+project = 'Pixel Neural Networks'
+author = 'Tu Nombre'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',    # genera documentación a partir de docstrings
+    'sphinx.ext.napoleon',   # soporta Google / NumPy style docstrings
+    'sphinx.ext.viewcode',   # enlaces al código fuente
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,28 @@
+Pixel Neural Networks Documentation
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   model
+   train
+   Loader
+
+Module Reference
+================
+
+.. automodule:: model
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: train
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: Loader
+    :members:
+    :undoc-members:
+    :show-inheritance:
